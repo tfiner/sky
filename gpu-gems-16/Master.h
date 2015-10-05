9f3107f5e55033aeb8d9f98111afafbb0490a250
Tim Finer <tfiner@fastmail.fm>
2015-10-04 17:02:08 -0700

    Porting to OSX.

--- gpu-gems-16/Master.h
+++ gpu-gems-16/Master.h
@@ -1,6 +1,11 @@
-#define _CRTDBG_MAP_ALLOC
+#if defined(_MSC_VER)
+#   define _CRTDBG_MAP_ALLOC
+#endif
+
 
 #include <GL/glew.h>
-#include <GL/wglew.h>
 
+#if defined(WIN32)
+#   include <GL/wglew.h>
+#endif
 
